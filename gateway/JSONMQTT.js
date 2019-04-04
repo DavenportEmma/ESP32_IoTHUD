30fa4b0faa87f659e7aebcd52fd840eca1738f0a
ConorDavenport <cnrdavenport@gmail.com>
2019-04-04 21:48:52 +0100

    json files sending over mqtt

--- gateway/JSONMQTT.js
+++ gateway/JSONMQTT.js
@@ -8,15 +8,34 @@ console.log("Client Started");
 client.on('connect', connectCallback); //when a 'connect' event is received call the connectCallback listener function
 client.on('message', messageCallback);
 
-function connectCallback() {
+var msg = {
+	name : "rpi",
+	age : 10,
+	admin : true
+};
+
+function connectCallback()
+{
+	console.log("Client Connected");
   	// publish a message to a topic, topic1/test
   	//client.subscribe('topic1/test');
-  	client.subscribe('/topic/qos1');
-  	client.subscribe('/topic/qos0');
+  	client.subscribe('/topic/conor1',subscribeCallback);
+  	//client.subscribe('/topic/qos0');
+}
+
+function subscribeCallback()
+{
+	console.log("Subscribed to Topic");
 }
 
 function messageCallback(topic, message, packet)
 {
 	console.log(message.toString());
+	var JSONmsg = JSON.stringify(msg);
+	client.publish('/topic/conor0',JSONmsg,publishCallback);
 }
 
+function publishCallback()
+{
+	console.log("Published Message");
+}
