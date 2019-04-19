966761ef4697c6100e1a5f42f0212ae0c91b15e3
ConorDavenport <cnrdavenport@gmail.com>
2019-04-19 17:56:18 +0100

    nodejs removes big long text element from fhir message before sending

--- gateway/FHIRMQTT.js
+++ gateway/FHIRMQTT.js
@@ -2,23 +2,43 @@
 var mqtt    = require('mqtt');
 var client  = mqtt.connect('mqtt://broker.mqttdashboard.com');
 const readline = require('readline');
+var fs = require('fs');
+
+const r1 = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
 
 console.log("Starting Client");
 console.log("Client Started");
 
 client.on('connect', connectCallback); //when a 'connect' event is received call the connectCallback listener function
 client.on('message', messageCallback);
-
-const r1 = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-
 r1.on('line', lineCallback);
 
 function lineCallback(line)
 {
-    client.publish('/topic/conor0',line,publishCallback);
+	if(line == "john")
+	{
+		client.publish('/topic/conor0',"john's code",publishCallback);
+		var content = fs.readFileSync('../FHIR_examples/glucose.json');
+	}
+	else if(line == "alice")
+	{
+		client.publish('/topic/conor0',"alice's code",publishCallback);
+		var content = fs.readFileSync('../FHIR_examples/co2.json');
+	
+	}
+	else
+	{
+		client.publish('/topic/conor0',"bob's code",publishCallback);
+		var content = fs.readFileSync('../FHIR_examples/excess.json');
+	}
+	var jsonContent = JSON.parse(content);
+	delete jsonContent.text;
+	console.log(jsonContent);
+	var newJSON = JSON.stringify(jsonContent);
+	client.publish('/topic/conor0',newJSON,publishCallback);
 }
 
 function connectCallback()
