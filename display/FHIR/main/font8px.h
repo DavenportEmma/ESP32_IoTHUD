754dc974d9c5c2a3ded40413d0edf59aacbd8a73
ConorDavenport <cnrdavenport@gmail.com>
2019-04-20 17:38:37 +0100

    added smaller font

---
+++ display/FHIR/main/font8px.h
@@ -0,0 +1,151 @@
+// 8x5px font
+// LSb is top of character
+// MSb is not used
+
+unsigned char blank8 [5] = {
+	
+};
+
+unsigned char one8 [5] = {
+
+};
+
+unsigned char two8 [5] = {
+
+};
+
+unsigned char three8 [5] = {
+
+};
+
+unsigned char four8 [5] = {
+
+};
+
+unsigned char five8 [5] = {
+
+};
+
+unsigned char six8 [5] = {
+
+};
+
+unsigned char seven8 [5] = {
+
+};
+
+unsigned char eight8 [5] = {
+
+};
+
+unsigned char nine8 [5] = {
+
+};
+
+unsigned char zero8 [5] = {
+
+};
+
+unsigned char A8 [5] = {
+
+};
+
+unsigned char B8 [5] = {
+
+};
+
+unsigned char C8 [5] = {
+
+};
+
+unsigned char D8 [5] = {
+
+};
+
+unsigned char E8 [5] = {
+
+};
+
+unsigned char F8 [5] = {
+
+};
+
+unsigned char G8 [5] = {
+
+};
+
+unsigned char H8 [5] = {
+
+};
+
+unsigned char I8 [5] = {
+
+};
+
+unsigned char J8 [5] = {
+
+};
+
+unsigned char K8 [5] = {
+
+};
+
+unsigned char L8 [5] = {
+
+};
+
+unsigned char M8 [5] = {
+
+};
+
+unsigned char N8 [5] = {
+
+};
+
+unsigned char O8 [5] = {
+
+};
+
+unsigned char P8 [5] = {
+
+};
+
+unsigned char Q8 [5] = {
+
+};
+
+unsigned char R8 [5] = {
+
+};
+
+unsigned char S8 [5] = {
+
+};
+
+unsigned char T8 [5] = {
+
+};
+
+unsigned char U8 [5] = {
+
+};
+
+unsigned char V8 [5] = {
+
+};
+
+unsigned char W8 [5] = {
+
+};
+
+unsigned char X8 [5] = {
+
+};
+
+unsigned char Y8 [5] = {
+
+};
+
+unsigned char Z8 [5] = {
+
+};
