a07b60f57b3286ae2186d78139b80eb2dc5af31e
ConorDavenport <cnrdavenport@gmail.com>
2019-04-27 15:22:02 +0100

    updated comments

--- display/FHIR/main/main.c
+++ display/FHIR/main/main.c
@@ -74,41 +74,41 @@ struct data {
 #define WHITE                          1 ///< Draw 'on' pixels
 #define INVERSE                        2 ///< Invert pixels
 
-#define SSD1306_MEMORYMODE          0x20 ///< See datasheet
-#define SSD1306_COLUMNADDR          0x21 ///< See datasheet
-#define SSD1306_PAGEADDR            0x22 ///< See datasheet
-#define SSD1306_SETCONTRAST         0x81 ///< See datasheet
-#define SSD1306_CHARGEPUMP          0x8D ///< See datasheet
-#define SSD1306_SEGREMAP            0xA0 ///< See datasheet
-#define SSD1306_DISPLAYALLON_RESUME 0xA4 ///< See datasheet
-#define SSD1306_DISPLAYALLON        0xA5 ///< Not currently used
-#define SSD1306_NORMALDISPLAY       0xA6 ///< See datasheet
-#define SSD1306_INVERTDISPLAY       0xA7 ///< See datasheet
-#define SSD1306_SETMULTIPLEX        0xA8 ///< See datasheet
-#define SSD1306_DISPLAYOFF          0xAE ///< See datasheet
-#define SSD1306_DISPLAYON           0xAF ///< See datasheet
-#define SSD1306_COMSCANINC          0xC0 ///< Not currently used
-#define SSD1306_COMSCANDEC          0xC8 ///< See datasheet
-#define SSD1306_SETDISPLAYOFFSET    0xD3 ///< See datasheet
-#define SSD1306_SETDISPLAYCLOCKDIV  0xD5 ///< See datasheet
-#define SSD1306_SETPRECHARGE        0xD9 ///< See datasheet
-#define SSD1306_SETCOMPINS          0xDA ///< See datasheet
-#define SSD1306_SETVCOMDETECT       0xDB ///< See datasheet
+#define SSD1306_MEMORYMODE          0x20 // horizontal, vertical, page addressing mode
+#define SSD1306_COLUMNADDR          0x21 // column start/end addresses
+#define SSD1306_PAGEADDR            0x22 // page start/end addresses
+#define SSD1306_SETCONTRAST         0x81 // contrast settings
+#define SSD1306_CHARGEPUMP          0x8D // 
+#define SSD1306_SEGREMAP            0xA0 //
+#define SSD1306_DISPLAYALLON_RESUME 0xA4 //
+#define SSD1306_DISPLAYALLON        0xA5 // 
+#define SSD1306_NORMALDISPLAY       0xA6 //
+#define SSD1306_INVERTDISPLAY       0xA7 //
+#define SSD1306_SETMULTIPLEX        0xA8 //
+#define SSD1306_DISPLAYOFF          0xAE //
+#define SSD1306_DISPLAYON           0xAF //
+#define SSD1306_COMSCANINC          0xC0 // 
+#define SSD1306_COMSCANDEC          0xC8 //
+#define SSD1306_SETDISPLAYOFFSET    0xD3 //
+#define SSD1306_SETDISPLAYCLOCKDIV  0xD5 //
+#define SSD1306_SETPRECHARGE        0xD9 //
+#define SSD1306_SETCOMPINS          0xDA //
+#define SSD1306_SETVCOMDETECT       0xDB //
 
-#define SSD1306_SETLOWCOLUMN        0x00 ///< Not currently used
-#define SSD1306_SETHIGHCOLUMN       0x10 ///< Not currently used
-#define SSD1306_SETSTARTLINE        0x40 ///< See datasheet
+#define SSD1306_SETLOWCOLUMN        0x00 //  
+#define SSD1306_SETHIGHCOLUMN       0x10 // 
+#define SSD1306_SETSTARTLINE        0x40 // 
 
-#define SSD1306_EXTERNALVCC         0x01 ///< External display voltage source
-#define SSD1306_SWITCHCAPVCC        0x02 ///< Gen. display voltage from 3.3V
+#define SSD1306_EXTERNALVCC         0x01 // External display voltage source
+#define SSD1306_SWITCHCAPVCC        0x02 // Gen. display voltage from 3.3V
 
-#define SSD1306_RIGHT_HORIZONTAL_SCROLL              0x26 ///< Init rt scroll
-#define SSD1306_LEFT_HORIZONTAL_SCROLL               0x27 ///< Init left scroll
-#define SSD1306_VERTICAL_AND_RIGHT_HORIZONTAL_SCROLL 0x29 ///< Init diag scroll
-#define SSD1306_VERTICAL_AND_LEFT_HORIZONTAL_SCROLL  0x2A ///< Init diag scroll
-#define SSD1306_DEACTIVATE_SCROLL                    0x2E ///< Stop scroll
-#define SSD1306_ACTIVATE_SCROLL                      0x2F ///< Start scroll
-#define SSD1306_SET_VERTICAL_SCROLL_AREA             0xA3 ///< Set scroll range
+#define SSD1306_RIGHT_HORIZONTAL_SCROLL              0x26 // Init rt scroll
+#define SSD1306_LEFT_HORIZONTAL_SCROLL               0x27 // Init left scroll
+#define SSD1306_VERTICAL_AND_RIGHT_HORIZONTAL_SCROLL 0x29 // Init diag scroll
+#define SSD1306_VERTICAL_AND_LEFT_HORIZONTAL_SCROLL  0x2A // Init diag scroll
+#define SSD1306_DEACTIVATE_SCROLL                    0x2E // Stop scroll
+#define SSD1306_ACTIVATE_SCROLL                      0x2F // Start scroll
+#define SSD1306_SET_VERTICAL_SCROLL_AREA             0xA3 // Set scroll range
 
 uint8_t *buffer;
 int8_t vccstate;
@@ -339,15 +339,30 @@ void display()
     i2c_cmd_link_delete(cmd);
 }
 
+// draw a single pixel on the display
+// param x          x coordinates, 0 - 127
+// param y          y coordinates, 0 - 63
+// param colour     white, black, or inverse the current pixel colour
 void drawPixel(int16_t x, int16_t y, uint16_t colour)
 {
+    // if the pixel is within the bounds of the display
 	if((x >= 0) && (x < WIDTH && (y >= 0) && (y < HEIGHT))) 
 	{
 	    switch(colour) 
 	    {
-	    	case WHITE:   buffer[x + (y/8)*WIDTH] |=  (1 << (y&7)); break;
-	    	case BLACK:   buffer[x + (y/8)*WIDTH] &= ~(1 << (y&7)); break;
-	    	case INVERSE: buffer[x + (y/8)*WIDTH] ^=  (1 << (y&7)); break;
+            // if colour is white assert the pixel
+            // OR 1 with the selected pixel
+	    	case WHITE:   
+                buffer[x + (y/8)*WIDTH] |=  (1 << (y&7)); break;
+            // if colour is black de-assert the pixel
+            // AND 0 with the selected pixel
+	    	case BLACK:   
+                buffer[x + (y/8)*WIDTH] &= ~(1 << (y&7)); break;
+            // if colour is inverse swap pixel colour
+            // XOR 1 with selected pixel, if pixel is white it goes black and vice versa
+	    	case INVERSE: 
+            buffer[x + (y/8)*WIDTH] ^=  (1 << (y&7)); break;
+
         }
 	}
 }
@@ -444,13 +459,16 @@ void drawChar8(unsigned char c[5], int16_t x, int16_t y)
 // draw a character from the font16px.h file
 void drawChar16(unsigned char c[10][2], int16_t x, int16_t y)
 {
+    // increment through the character array
     int i, j;
     for(i = 0; i < 2; i++)
     {
         for(j = 0; j < 10; j++)
         {
+            // if the character exceeds the buffer size
             if((j + x) + (128 * (i + y)) > 1023)
                 break;
+            // write the current byte to the buffer
             buffer[(j + x) + (128 * (i + y))] = c[j][i];
         }
     }
